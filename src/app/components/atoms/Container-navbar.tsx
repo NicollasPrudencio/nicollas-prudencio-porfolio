f0f6d347a79c7565eeece4610557f921acc8a81b
NicollasPrudencio <nicollas_ap37@hotmail.com>
2024-12-25 20:01:41 -0300

    update(atoms): cria atomo Container for Navbar

--- src/app/components/atoms/Container-navbar.tsx
+++ src/app/components/atoms/Container-navbar.tsx
@@ -1,7 +1,17 @@
-const Container = ({ children }: { children: React.ReactNode }) => (
-    <div className="flex flex-col justify-center items-center w-12 h-12 cursor-pointer">
-      {children}
-    </div>
-  );
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  onClick?: () => void; // Adicione o tipo para a propriedade onClick (opcional)
+}
+
+const Container: React.FC<ContainerProps> = ({ children, onClick }) => (
+  <div
+    className="flex flex-col justify-center items-center w-12 h-12 cursor-pointer"
+    onClick={onClick} // Passa o evento onClick para o elemento <div>
+  >
+    {children}
+  </div>
+);
 
 export default Container;
