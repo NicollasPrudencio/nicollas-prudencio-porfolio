cd53b576ab164b584cf1b73cb463357d388d7965
NicollasPrudencio <nicollas_ap37@hotmail.com>
2024-12-27 10:42:39 -0300

    update(atoms): atualiza a atomo Line do hamburguer

--- src/app/components/atoms/Line-hamburguer.tsx
+++ src/app/components/atoms/Line-hamburguer.tsx
@@ -1,3 +1,41 @@
-const Line = () => <div className="w-10 h-1 bg-black"></div>;
+type LineProps = {
+  /**
+   * Define a grossura da linha em pixels.
+   * @default 2
+   */
+  thickness?: number;
+
+  /**
+   * Define a largura da linha em pixels.
+   * @default 40
+   */
+  width?: number;
+
+  /**
+   * Define o espaçamento entre as linhas (margin-bottom) em pixels.
+   * @default 4
+   */
+  spacing?: number;
+};
+
+/**
+ * Um componente de linha reutilizável com estilos personalizáveis.
+ * 
+ * @param {number} [thickness=2] - Define a grossura da linha em pixels (padrão: 2).
+ * @param {number} [width=40] - Define a largura da linha em pixels (padrão: 40).
+ * @param {number} [spacing=4] - Define o espaçamento entre as linhas em pixels (padrão: 4).
+ * 
+ * @returns {JSX.Element} Um componente de linha estilizado.
+ */
+const Line = ({ thickness = 2, width = 40, spacing = 4 }: LineProps) => (
+  <div
+    className={`bg-black rounded-full`}
+    style={{
+      height: `${thickness}px`, // Altura da linha
+      width: `${width}px`, // Largura da linha
+      marginBottom: `${spacing}px`, // Espaçamento entre as linhas
+    }}
+  ></div>
+);
 
 export default Line;
