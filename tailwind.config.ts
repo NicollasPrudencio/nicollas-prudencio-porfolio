e785390dce85ae9df4057cf285ec65494a3ec916
NicollasPrudencio <nicollas_ap37@hotmail.com>
2024-12-27 10:46:31 -0300

    update(tailwind): adiciona estilos no arquivo do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,14 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      translate: {
+        "2": "0.5rem", // Ajusta translate-y-2 para 8px
+        "6px": "6px", // Adiciona uma tradução de 6px (usada nas transformações)
+      },
+      rotate: {
+        45: "45deg", // Garante que rotate-45 funcione corretamente
+        "-45": "-45deg", // Garante que -rotate-45 funcione corretamente
+      },
     },
   },
   plugins: [],
